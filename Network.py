46d8a03d60c91b4cfb09052e713b70f4ff5cc80f
Gaganpreet Khurana <giftykhurana0@gmail.com>
2019-11-14 14:04:29 +0530

    Instructions added

--- Network.py
+++ Network.py
@@ -8,7 +8,7 @@ class network:
     Initialises the client. Add the host ipv4 address.
     """
     def __init__(self):
-        self.host = socket.gethostbyname(socket.gethostname())  # For this to work on your machine this must be equal to the ipv4 address of the machine running the server
+        self.host = '172.31.70.162'#socket.gethostbyname(socket.gethostname())  # For this to work on your machine this must be equal to the ipv4 address of the machine running the server
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # You can find this address by typing ipconfig in CMD and copying the ipv4 address. Again this must be the servers
         # ipv4 address. This feild will be the same for all your clients.
